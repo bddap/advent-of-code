63518c63915a55de994ed35cb44556a9e9c6a8e5
Andrew Dirksen <andrew@dirksen.com>
2021-12-02 09:24:33 -0800

    2020 day15 part2

--- 2020/src/day15.rs
+++ 2020/src/day15.rs
@@ -1,62 +1,88 @@
-use std::collections::BTreeMap;
-
-fn parse(input: &str) -> Vec<u64> {
+fn parse(input: &str) -> Vec<usize> {
     input.split(',').map(|s| s.parse().unwrap()).collect()
 }
 
+#[aoc_runner_derive::aoc(day15, part1)]
+pub fn solve1(input: &str) -> usize {
+    let input = parse(input);
+    let iter = Iter::from_list(&input);
+    input.into_iter().chain(iter).skip(2019).next().unwrap()
+}
+
+// there's probably a faster clever way to solve this one
+// this method is bruteish (732.538539ms)
+#[aoc_runner_derive::aoc(day15, part2)]
+pub fn solve2(input: &str) -> usize {
+    let input = parse(input);
+    let iter = Iter::from_list(&input);
+    input
+        .into_iter()
+        .chain(iter)
+        .skip(30000000 - 1)
+        .next()
+        .unwrap()
+}
+
+struct SmallUintMap {
+    map: Vec<Option<usize>>,
+}
+
+impl SmallUintMap {
+    fn new() -> Self {
+        SmallUintMap { map: Vec::new() }
+    }
+
+    fn insert(&mut self, k: usize, v: usize) {
+        if self.map.len() <= k {
+            let toadd = (k * 2) - self.map.len() + 1;
+            self.map.extend((0..toadd).map(|_| None));
+        }
+        self.map[k] = Some(v);
+    }
+
+    fn get(&self, k: usize) -> Option<usize> {
+        self.map.get(k).cloned().flatten()
+    }
+}
+
 struct Iter {
-    prev: u64,
-    history: BTreeMap<u64, u64>,
-    idx: u64,
+    prev: usize,
+    history: SmallUintMap,
+    idx: usize,
 }
 
 impl Iter {
-    fn from_list(v: &[u64]) -> Self {
+    fn from_list(v: &[usize]) -> Self {
         assert!(!v.is_empty());
 
-        let mut history = BTreeMap::<u64, u64>::new();
+        let mut history = SmallUintMap::new();
 
-        assert_eq!(v[0..v.len() - 1].len(), v.len() - 1);
         for (i, n) in v[0..v.len() - 1].iter().enumerate() {
-            history.insert(*n, i as u64);
+            history.insert(*n, i);
         }
 
         Iter {
             prev: *v.last().unwrap(),
             history,
-            idx: v.len() as u64 - 1,
+            idx: v.len() - 1,
         }
     }
 }
 
 impl Iterator for Iter {
-    type Item = u64;
+    type Item = usize;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.idx += 1;
         let ret = self
             .history
-            .get(&self.prev)
+            .get(self.prev)
             .map(|n| self.idx - n - 1)
             .unwrap_or(0);
         self.history.insert(self.prev, self.idx - 1);
         self.prev = ret;
         Some(ret)
     }
-}
-
-#[aoc_runner_derive::aoc(day15, part1)]
-pub fn solve1(input: &str) -> u64 {
-    let input = parse(input);
-    let iter = Iter::from_list(&input);
-
-    input.into_iter().chain(iter).skip(2019).next().unwrap()
-}
-
-#[aoc_runner_derive::aoc(day15, part2)]
-pub fn solve2(input: &str) -> u32 {
-    parse(input);
-    todo!();
 }
 
 #[cfg(test)]
@@ -82,5 +108,17 @@ mod tests {
         assert_eq!(solve1("2,3,1"), 78);
         assert_eq!(solve1("3,2,1"), 438);
         assert_eq!(solve1("3,1,2"), 1836);
+    }
+
+    #[test]
+    #[ignore] // takes too long
+    fn tsolve2() {
+        assert_eq!(solve2("0,3,6"), 175594);
+        assert_eq!(solve2("1,3,2"), 2578);
+        assert_eq!(solve2("2,1,3"), 3544142);
+        assert_eq!(solve2("1,2,3"), 261214);
+        assert_eq!(solve2("2,3,1"), 6895259);
+        assert_eq!(solve2("3,2,1"), 18);
+        assert_eq!(solve2("3,1,2"), 362);
     }
 }

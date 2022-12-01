e7c0f6d620f9e91848cb15a1f5f848d491fa2066
Andrew Dirksen <andrew@dirksen.com>
2022-12-01 07:21:25 -0800

    I'm going to say silly things in these commit messages this year.

--- 2022/src/d01p1.rs
+++ 2022/src/d01p1.rs
@@ -1,5 +1,14 @@
 #[macro_rules_attribute::apply(challenge)]
 #[aoc(2022, 1, 1)]
-fn run(_inp: &str) -> anyhow::Result<usize> {
-    unimplemented!()
+fn run(inp: &str) -> anyhow::Result<usize> {
+    inp.split("\n\n")
+        .map(|section| {
+            section
+                .split("\n")
+                .filter(|line| !line.is_empty())
+                .map(|line| line.parse::<usize>().unwrap())
+                .sum::<usize>()
+        })
+        .max()
+        .ok_or(anyhow::anyhow!("no input numbers"))
 }
--- 2022/src/d01p2.rs
+++ 2022/src/d01p2.rs
@@ -1,5 +1,18 @@
 #[macro_rules_attribute::apply(challenge)]
 #[aoc(2022, 1, 2)]
-fn run(_inp: &str) -> anyhow::Result<usize> {
-    unimplemented!()
+fn run(inp: &str) -> anyhow::Result<usize> {
+    let mut ls: Vec<usize> = inp
+        .split("\n\n")
+        .map(|section| {
+            section
+                .split("\n")
+                .filter(|line| !line.is_empty())
+                .map(|line| line.parse::<usize>().unwrap())
+                .sum::<usize>()
+        })
+        .collect();
+    ls.sort();
+    ls.reverse();
+    let top3 = ls.get(0..3).ok_or(anyhow::anyhow!("not enough input"))?;
+    Ok(top3.iter().sum::<usize>())
 }

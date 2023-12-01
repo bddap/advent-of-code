d27133ec34751be18910f4ee9825bee03ad8cf88
Andrew Dirksen <andrew@dirksen.com>
2023-12-01 09:20:41 -0800

    boilerblate for year 2023

---
+++ 2023/src/day09part2.rs
@@ -0,0 +1,14 @@
+// #[bddap_aoc::register(2023, 9, 2)]
+// fn solve(input: &str) -> String {
+//     unimplemented!()
+// }
+//
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn test() {
+//         assert_eq!(solve.run(""), "");
+//     }
+// }
---
+++ 2023/src/day13part1.rs
@@ -0,0 +1,14 @@
+// #[bddap_aoc::register(2023, 13, 1)]
+// fn solve(input: &str) -> String {
+//     unimplemented!()
+// }
+//
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn test() {
+//         assert_eq!(solve.run(""), "");
+//     }
+// }
